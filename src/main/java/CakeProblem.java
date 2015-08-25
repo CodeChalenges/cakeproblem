6d7d09b409f0bc1e22cd0f8b090845fa7d61c7e7
Mauricio Klein <mauricio.klein@zenvia.com>
2015-08-25 15:07:56 -0300

    Algorithm adjustments.

--- src/main/java/CakeProblem.java
+++ src/main/java/CakeProblem.java
@@ -27,21 +27,27 @@ public class CakeProblem {
      *
      * Input:
      *  @param acc the sum of layers already consumed
-     *  @param subsequence the remaining layers
+     *  @param input the remaining layers
      *
      * Output:
      *  @return Integer the best local solution: left, right or none
      * */
-    private static Integer cake(final int acc, final int[] subsequence) {
-        final int subsLength = subsequence.length;
+    private static Integer cake(final int acc, final int[] input) {
+        final int nlayers          = input.length,
+                  topLayerValue    = input[0],
+                  bottomLayerValue = input[nlayers-1];
 
-        if (subsLength == 1) {
-            return acc + subsequence[0];
+        if (nlayers == 1) {
+            return acc + topLayerValue;
         }
         else{
-            int accLeft  = cake(acc + subsequence[0], Arrays.copyOfRange(subsequence, 1, subsLength));
-            int accRight = cake(acc + subsequence[subsLength-1], Arrays.copyOfRange(subsequence, 0, subsLength-1));
-            return Math.max(acc, Math.max(accLeft, accRight));
+            int accLeft  = cake(acc + topLayerValue   , Arrays.copyOfRange(input, 1, nlayers  ));
+            int accRight = cake(acc + bottomLayerValue, Arrays.copyOfRange(input, 0, nlayers-1));
+            return max(acc, accLeft, accRight);
         }
+    }
+
+    private static Integer max(int x, int y, int z){
+        return Math.max(x, Math.max(y, z));
     }
 }
